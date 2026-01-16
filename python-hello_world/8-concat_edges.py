69e0cab68681b4cbfb37116af1cac648052f6c7c
Joshua <joshgonzalez027@gmail.com>
2026-01-16 01:05:35 -0400

    Fix added

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-str1 = "object-oriented programming with Python"
-str2 = str1[:18]
-str3 = str1[19:]
-str = str2 + str3
+str = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
 print(str)
