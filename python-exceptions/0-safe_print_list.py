576dadd3e213e7e75e611ecd282fb34cbf917e2f
Joshua <joshgonzalez027@gmail.com>
2026-01-23 14:51:04 -0400

    Add safe_print_list function using try/except

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Prints x elements of a list safely using try/except
+def safe_print_list(my_list=[], x=0):
+    count = 0
+
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            count += 1
+        except Exception:
+            break
+
+    print()
+    return count
