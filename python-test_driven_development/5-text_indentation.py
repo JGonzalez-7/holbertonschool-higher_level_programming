1137e2e8b02aea69fdc0caf800b505c22917522c
Joshua <joshgonzalez027@gmail.com>
2026-01-25 14:01:40 -0400

    Fix text_indentation spacing for hidden tests

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-# Prints text with indentation after specific punctuation
+# Prints text with two new lines after '.', '?' and ':'
 
 """Defines text_indentation(text)."""
 
 
 def text_indentation(text):
-    """Print text with two new lines after ., ? and :."""
+    """Print text with two new lines after '.', '?' and ':'."""
 
     if type(text) is not str:
         raise TypeError("text must be a string")
 
-    line = ""
-    for char in text:
-        line += char
-        if char in ".?:":
-            print(line.strip())
-            print()
-            line = ""
+    at_line_start = True
 
-    if line:
-        print(line.strip(), end="")
+    for char in text:
+        if char == " " and at_line_start:
+            continue
+
+        print(char, end="")
+
+        if char in ".?:":
+            print("\n")
+            at_line_start = True
+        else:
+            at_line_start = False
