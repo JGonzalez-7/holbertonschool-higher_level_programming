c8b2518151aa9a62bd9c15f5cecaa973bce3f47a
Joshua <joshgonzalez027@gmail.com>
2026-02-03 15:17:08 -0400

    Fixed pycodestyle

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module that checks if an object is an instance of a class or its subclasses."""
+"""Module to check instance or inherited class."""
 
 
 def is_kind_of_class(obj, a_class):
