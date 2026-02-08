cf402e32e240d1133ec973342a285ae7560495f4
Joshua <joshgonzalez027@gmail.com>
2026-02-08 13:39:48 -0400

    Handle negative radius in Circle by normalizing input

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -26,7 +26,7 @@ class Circle(Shape):
     """Circle defined by a radius."""
 
     def __init__(self, radius):
-        self.radius = radius
+        self.radius = abs(radius)
 
     def area(self):
         return math.pi * (self.radius ** 2)
