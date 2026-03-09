a17a5c568ace4b8cca0aefa97dc4a0e18e2222d7
Joshua <joshgonzalez027@gmail.com>
2026-03-08 22:33:38 -0400

    added script to create read-only MySQL user

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,12 @@
+-- create the database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- create the user if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+
+-- give the user only SELECT permission on the database
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- reload privileges
+FLUSH PRIVILEGES;
