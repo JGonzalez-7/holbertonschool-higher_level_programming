ec08a037406ec4dbbb919fa21fbf1c675a8a951f
Joshua <joshgonzalez027@gmail.com>
2026-03-10 14:03:05 -0400

    fixed query to filter states starting with uppercase N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -4,7 +4,6 @@
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
@@ -15,7 +14,8 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
 
     rows = cur.fetchall()
     for row in rows:
